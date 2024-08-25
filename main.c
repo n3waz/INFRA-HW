7b5b048ced6632591d4cc2823252c691a003b382
kat0 <lhk@cesar.school>
2024-08-25 00:33:51 -0300

    Iniciando Repositório

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+
+ * Criado em 25 de Agosto 2024
+ * Autor: Luan Kato
+ * Questão 1 - 25/08/2024 00:55
+
+**/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+}
